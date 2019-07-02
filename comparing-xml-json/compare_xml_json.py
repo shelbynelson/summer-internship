f92f8d168218860e627410385f4d30bd74ccd79a
shelbynelson <shelbeezy@email.arizona.edu>
2019-07-02 13:23:59 -0700

    it prints

--- comparing-xml-json/compare_xml_json.py
+++ comparing-xml-json/compare_xml_json.py
@@ -40,6 +40,7 @@ def main():
     args = get_args()
     files_ = args.files
     
+    #---------------------------This section reads the files and gets the biosample names--------------------------------------------------
     biosample_re = re.compile('^bio([\s/-])?sample(s)?$')
     file_count = 1
     for tsv_file in files_:
@@ -47,29 +48,24 @@ def main():
         if not os.path.isfile(tsv_file):
             die('input file "{}" is not a file'.format(tsv_file))
         else:
-            #print(tsv_file)             #file name 
             with open(tsv_file) as f:
                 reader = csv.DictReader(f, dialect='excel-tab')
                 d_list = []
                 for line in reader:
                     d_list.append(line)
-                #print(d_list)
                 for sample in d_list:
-                    #print(sample)                      #All of indiv sample data
                     for col_header in sample:
-                        #print(col_header)              #column header
-                        #print(sample[col_header])      #column value
-                        
                         if biosample_re.match(col_header.lower()):
-                            #print(col_header)           #prints biosample in the file's form
-                            #print(sample[col_header])    #sample name
                             samples_d.append(sample[col_header])
         if file_count == 1:
             first_samples_d = samples_d
+            first_file_name = tsv_file
+        elif file_count == 2:
+            sec_file_name = tsv_file
 
         file_count = file_count + 1
-        
-        #break
+    
+    #--------------This section compares the two lists-------------------------------------------------------------------------
     first_file_d = set(first_samples_d)
     sec_file_d = set(samples_d)
 
@@ -77,16 +73,47 @@ def main():
     all_samples = first_file_d.union(sec_file_d)
     not_in_both = all_samples - in_both
     
+    #print(first_file_d)                #Prints all of the samples in the first file
+    f1_count = len(first_file_d)        #The count of samples in the first file
+    #print(sec_file_d)                  #Prints all of the samples in the second file
+    f2_count = len(sec_file_d)          #The count of samples in the second file
+    #print(in_both)                     #Prints all of the samples that are intersecting/in both of the files
+    #print(len(in_both))                #Prints the number of samples that are the same in both files
+    #print(all_samples)                 #Prints the union or all of the samples in
+    #print(len(all_samples))            #Prints the total number of samples in both files
+    #print(not_in_both)                 #Prints the samples that are not the same in both files
+    #print(len(not_in_both))            #Printd the number of samples that are not the same in both files
 
-    print(in_both)
-    print(len(in_both))
-    print('\n')
-    print(all_samples)
-    print(len(all_samples))
-    print('\n')
-    print(not_in_both)
-    print(len(not_in_both))
+    nf1_counter = 0 
+    nf2_counter = 0
+    in_1_not_2 = []
+    in_2_not_1 = []
+    for samp in not_in_both:            #Compares samples in "not_in_both" to samples in the first and second files
+        for biosample in first_file_d:
+            if biosample == samp:
+                in_1_not_2.append(samp)   #Appends the samples to a list that are in the first file and are not in the second file
+                nf1_counter+=1
+        for biosample in sec_file_d:
+            if biosample == samp:
+                in_2_not_1.append(samp)     #Appends the samples to a list that are in the second file and are not in the first file
+                nf2_counter+=1
 
-# --------------------------------------------------
+    #-----------This section prints the outcome of the program-----------------------------------------------------------------------------
+   
+    #Prints the total number of samples in each file
+    print('\nThere are {} samples in {}\n\nThere are {} samples in {}\n'.format(f1_count,first_file_name,f2_count, sec_file_name))
+
+    #Prints the number of samples that are in the first file and not in second, the file names, and a list of the samples
+    print('There are {} samples in {} file that are not found in {} file: '.format(nf1_counter, first_file_name, sec_file_name))
+    if in_1_not_2:
+        print(in_1_not_2)
+
+    #Prints the number of samples that are in the second file and not in first, the file names, and a list of the samples
+    print('\nThere are {} samples in {} file that are not found in {} file:'.format(nf2_counter,sec_file_name, first_file_name))
+    if in_2_not_1:
+        print(in_2_not_1)
+    print('\n')
+        
+# ------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
