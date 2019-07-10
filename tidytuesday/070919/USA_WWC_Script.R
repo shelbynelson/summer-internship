293b214b3066c6fd91f08d121ef2d4830e88fb87
Shelby Nelson <shelbeezy@email.arizona.edu>
2019-07-10 15:49:00 -0700

    fixed script

--- tidytuesday/070919/USA_WWC_Script.R
+++ tidytuesday/070919/USA_WWC_Script.R
@@ -2,6 +2,8 @@
 #071019
 #Women's World Cup TidyTuesday 
 
+library(tidyverse)
+
 wwc_outcomes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/wwc_outcomes.csv")
 
 ggplot(data = filter(wwc_outcomes,team=="USA"))+geom_point(mapping=aes(x=year,y=score),color="red")+geom_smooth(mapping=aes(x=year,y=score),se=FALSE)+labs(title="USA's Women's World Cup Scores",subtitle= "1991-2019",x="Year",y="Score")
